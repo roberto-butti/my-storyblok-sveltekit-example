8e2dca7cf9cfd394dcabd6de04d450549edf07a4
Roberto Butti <roberto.butti@gmail.com>
2023-04-30 15:26:21 +0200

    article page
    Article page with:
    - richtext resolver
    - rich text resolver for nested component

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -4,16 +4,75 @@
 {#key data}
     <div>
         {#if data.story}
-            <StoryblokComponent blok="{data.story.content}" />
+            <section class="hero is-info is-medium is-bold">
+                <div class="hero-body">
+                    <div class="container has-text-centered">
+                        <h1 class="title">{data.story.content.title}</h1>
+                    </div>
+                </div>
+            </section>
+            <div class="container">
+                <!-- START ARTICLE FEED -->
+                <section class="articles">
+                    <div class="column is-8 is-offset-2">
+                        <!-- START ARTICLE -->
+                        <div class="card article">
+                            <div class="card-content">
+                                <div class="media">
+                                    <div
+                                        class="media-content has-text-centered"
+                                    >
+                                        <p class="title article-title">
+                                            {data.story.content.subtitle}
+                                        </p>
+                                        <div class="tags has-addons level-item">
+                                            <span class="tag is-rounded"
+                                                >{data.story.created_at}</span
+                                            >
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="content article-body">
+                                    {#each data.story.content.content.content as node, i}
+                                        {#if node.type === "blok"}
+                                            {#each node.attrs.body as bodyItem, idxBody}
+                                                <StoryblokComponent
+                                                    blok="{bodyItem}"
+                                                />
+                                            {/each}
+                                        {:else}
+                                            {@html renderNode(node)}
+                                        {/if}
+                                    {/each}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+            <!-- END ARTICLE -->
         {/if}
     </div>
 {/key}
 
 <script>
 import { onMount } from "svelte";
+import { renderRichText } from "@storyblok/svelte";
 import { useStoryblokBridge, StoryblokComponent } from "@storyblok/svelte";
 
 export let data;
+
+/**
+ * @param {{ type: string; }} node
+ */
+function renderNode(node) {
+    return renderRichText({
+        type: "doc",
+        content: [node],
+    });
+}
+$: articleHTML = renderRichText(data.story.content.content);
 
 onMount(() => {
     if (data.story) {
