e1f0a14b4d7888e7660eb76d5740dddf3cf747ec
Roberto Butti <roberto.butti@gmail.com>
2023-05-27 09:05:15 +0200

    Update Page.svelte

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -1,10 +1,10 @@
-{#key blok}
+
     <div use:storyblokEditable="{blok}" class="container">
         {#each blok.body as item}
             <StoryblokComponent blok="{item}" />
         {/each}
     </div>
-{/key}
+
 
 <script>
 import { storyblokEditable, StoryblokComponent } from "@storyblok/svelte";
