111d6040bd3528de18a7e8b5613909ee1c76839a
Roberto Butti <roberto.butti@gmail.com>
2023-05-27 09:05:06 +0200

    Grid Svelte animation

--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -1,6 +1,7 @@
-<div use:storyblokEditable="{blok}" class="columns mt-4">
-    {#each blok.columns as item}
-        <div class="column">
+<div  class="columns mt-4">
+    {#each blok.columns as item (item._uid)}
+
+        <div class="column" animate:flip={options}>
             <StoryblokComponent blok="{item}" />
         </div>
     {/each}
@@ -8,7 +9,9 @@
 
 <script>
 import { storyblokEditable, StoryblokComponent } from "@storyblok/svelte";
-
+import { flip } from 'svelte/animate';
+import {bounceInOut} from 'svelte/easing';
+const options= {}
 /**
  * @type any
  */
