c04d70e4f4d51fc9e42d7c04159541a94dc00619
Roberto Butti <roberto.butti@gmail.com>
2023-05-30 22:21:27 +0200

    Update +page.svelte

--- src/routes/serverside/+page.svelte
+++ src/routes/serverside/+page.svelte
@@ -5,20 +5,7 @@
 </div>
 
 <script>
-import { onMount } from "svelte";
-import { useStoryblokBridge, StoryblokComponent } from "@storyblok/svelte";
+import { StoryblokComponent } from "@storyblok/svelte";
 
 export let data;
-
-onMount(() => {
-    if (data.serverstory) {
-        useStoryblokBridge(
-            data.serverstory.id,
-            (newStory) => (data.serverstory = newStory),
-            {
-                resolveRelations: ["popular-articles.articles"],
-            }
-        );
-    }
-});
 </script>
